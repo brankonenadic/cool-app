163ef521dc87563d15b7ac8bf4c6efdea7adde4c
brankonenadic <57287210+brankonenadic@users.noreply.github.com>
2021-11-03 11:13:25 +0100

    Update

---
+++ src/components/layout/Navbar.js
@@ -0,0 +1,42 @@
+import React, {useState, useEffect}  from 'react';
+import classes from './Navbar.module.css';
+
+const Navbar = () => {
+    const [toggleMenu, setToggleMenu] = useState(false)
+    const [screenWidth, setScreenWidth] = useState(window.innerWidth)
+  
+  
+    const toggleNav = () => {
+      setToggleMenu(!toggleMenu)
+    }
+  
+    useEffect(() => {
+  
+      const changeWidth = () => {
+        setScreenWidth(window.innerWidth);
+      }
+  
+      window.addEventListener('resize', changeWidth)
+  
+      return () => {
+          window.removeEventListener('resize', changeWidth)
+      }
+  
+    }, [])
+    return (
+        <nav>
+        {(toggleMenu || screenWidth > 500) && (
+        <ul className={classes.list}>
+        <li className={classes.items}>Home</li>
+        <li className={classes.items}>Services</li>
+        <li className={classes.items}>Contact</li>
+        
+      </ul>
+        )}
+  
+        <button onClick={toggleNav} className={classes.btn}><i className="fas fa-bars fa-2x"></i></button>
+      </nav>
+    )
+}
+
+export default Navbar
