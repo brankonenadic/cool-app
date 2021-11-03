ed2ce07349705efb57da39d86949780d4ae68692
brankonenadic <57287210+brankonenadic@users.noreply.github.com>
2021-11-03 11:20:05 +0100

    Update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
+import { Fragment } from 'react';
+import { Switch, Route, Redirect } from 'react-router-dom';
+import Footer from './components/layout/Footer';
 import Navbar from './components/layout/Navbar';
 
 function App() {
   return (
-    <div>
-     <Navbar />
-    </div>
+    <Fragment>
+      <Navbar />
+      <Switch>
+        <Route path="/" exact>
+          <Redirect to="/home" />
+        </Route>
+      </Switch>
+      <Footer />
+    </Fragment>
   );
 }
 
