4256408f60ba45da35b297bcae0a67156e73200d
brankonenadic <57287210+brankonenadic@users.noreply.github.com>
2022-01-22 14:13:45 +0100

    Update

---
+++ src/components/ui/Card.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import classes from './Card.module.css'
+
+const Card = () => {
+  return (
+    <div className={classes.cardItem}>
+    <div> </div>
+    <h1> {name} </h1>
+    <p></p>
+  </div>
+);
+  
+};
+
+export default Card;
